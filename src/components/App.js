2ebdd0493f6fa6b7540e0265ff6e191cb4fd0c44
arieldavis22 <aadavisbusiness@gmail.com>
2020-04-23 22:30:56 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,54 @@ import React, { Component } from "react";
 import "../App.css";
 import Nav from "./Nav";
 import hogs from "../porkers_data";
-import HelloWorld from "./HelloWorld";
+import HogIndex from "./HogIndex";
 
 class App extends Component {
+
+  state = {
+    allHogs: hogs,
+    greeseFilter: false,
+    sortByName: false,
+    sortByWeight: false
+  }
+
+  handleGreeseToggle = () =>{
+    this.setState({
+      greeseFilter: !this.state.greeseFilter
+    })
+  }
+
+  handleNameToggle = () => {
+    this.setState({
+      sortByName: !this.state.sortByName
+    })
+  }
+
+  handleWeightToggle = () => {
+    this.setState({
+      sortByWeight: !this.state.sortByWeight
+    })
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <Nav />
-        <HelloWorld />
+        <Nav 
+        isGreesy={this.state.greeseFilter}
+        greeseFilter={this.handleGreeseToggle}
+        isSortedByName={this.state.sortByName}
+        nameSort={this.handleNameToggle}
+        isSortedByWeight={this.state.sortByWeight}
+        weightSort={this.handleWeightToggle}/>
+        <div>
+        <HogIndex 
+        allHogs={this.state.allHogs}
+        greeseFilter={this.state.greeseFilter}
+        isSortedByName={this.state.sortByName}
+        isSortedByWeight={this.state.sortByWeight}/>
+        </div>
       </div>
     );
   }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,22 @@
 import piggy from "../porco.png";
 import React from "react";
 
-const Nav = () => {
-  return (
-    <div className="navWrapper">
-      <span className="headerText">Hogwarts</span>
-      <div className="TwirlyPig">
-        <img src={piggy} className="App-logo" alt="piggy" />
+class Nav extends React.Component {
+
+  render() {
+    return (
+      <div className="navWrapper">
+        <button onClick={this.props.greeseFilter}>{this.props.isGreesy ? "Greese Filter: ON" : "Greese Filter: OFF"}</button>
+        <span className="headerText">Hogwarts</span>
+        <div className="TwirlyPig">
+          <img src={piggy} className="App-logo" alt="piggy" />
+        </div>
+        <span className="normalText">A React App for County Fair Hog Fans</span>
+          <button onClick={this.props.nameSort}>{this.props.isSortedByName ? "Name Sorted: ON" : "Name Sorted: OFF"}</button>
+        <button onClick={this.props.weightSort}>{this.props.isSortedByWeight ? "Weight Sorted: ON" : "Weight Sorted: OFF"}</button>
       </div>
-      <span className="normalText">A React App for County Fair Hog Fans</span>
-    </div>
-  );
+    );
+  }
 };
 
 export default Nav;
